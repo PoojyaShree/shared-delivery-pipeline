e9e6cdcf64f3e77d6b5ee0a6960599e8adb27dd6
PoojyaShree <87475087+PoojyaShree@users.noreply.github.com>
2022-01-17 18:46:49 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -6,6 +6,35 @@ def call(body) {
 
     pipeline {
        
-        
+        agent {
+            docker{ 
+                //image 'gaddamnarendra/maven:latest'
+            image 'gaddamnarendra/myprivaterepo:latest'
+            registryUrl 'https://registry.hub.docker.com'
+            registryCredentialsId 'DOCKER_CRDS'
+                  }
+              }
+              
+        stages {
+            stage('checkout git') {
+                steps {
+                    git branch: pipelineParams.branch, url: pipelineParams.scmUrl
+                }
+            }
+
+            stage('build') {
+                steps {
+                    sh 'mvn clean package -DskipTests=true'
+                }
+            }
+            stage ('test') {
+                steps {
+                    parallel (
+                        "unit tests": { sh 'mvn test' },
+                        "integration tests": { sh 'mvn integration-test' }
+                    )
+                }
+            }
+         }
     }
 }
