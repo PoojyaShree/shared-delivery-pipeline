79ff3e821b2d2a630b304bda4db647cbf7f56fb4
PoojyaShree <87475087+PoojyaShree@users.noreply.github.com>
2022-01-19 18:29:34 +0530

    Update mavenn.groovy

--- vars/mavenn.groovy
+++ vars/mavenn.groovy
@@ -1,1 +1,7 @@
-
+def call(body) {
+     def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+   def mavenGoals=pipelineParams.mavenGoals ?: 'clean package'
+   def M3_HOME= tool 'maven'
