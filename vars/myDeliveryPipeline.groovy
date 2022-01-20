5bd25c6d2153de2b3c62a90dbb41d9535118c690
PoojyaShree <87475087+PoojyaShree@users.noreply.github.com>
2022-01-20 11:08:45 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -3,12 +3,9 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = pipelineParams
     body()
-     def mvnGoals = pipelineParams.mvnGoals ?: 'clean package'
-     def mvnTest = pipelineParams.mvnTest ?: 'test'
-     def mvnintTest = pipelineParams.mvnintTest ?: 'integration-test'
-
+     
     pipeline {
-         //environment{ rtMaven=''}
+         environment{ rtMaven=''}
         agent any /*{
             docker{ 
                 //image 'gaddamnarendra/maven:latest'
@@ -28,34 +25,19 @@ def call(body) {
             stage('build') {
                 steps {
                     //sh 'mvn clean package -DskipTests=true'
-                    /* script {
-                            //def server = Artifactory.server('artifactory')
+                    script {                            
                                rtMaven = Artifactory.newMavenBuild()
-                               //rtMaven.resolver server: server, releaseRepo: 'libs-release', snapshotRepo: 'libs-snapshot'
-                                //rtMaven.deployer server: server, releaseRepo: 'libs-release-local', snapshotRepo: 'libs-snapshot-local'
                                 rtMaven.tool = 'maven'
                                  def buildInfo = rtMaven.run pom: 'pom.xml', goals: 'install'
-                                  //server.publishBuildInfo buildInfo
-                            }*/
-                     script{
-                     if("${env.BRANCH_NAME}" !='master'){
-                          mavenn{
-                               mavenGoals = "${mvnGoals}"
+                               }
                           }
-                     } 
                     }
-                }
-            }
             stage ('test') {
                 steps {
                      script{
                     parallel (
-                         mavenn{
-                              echo "unit tests:"+ { mavenGoals = "${mvnTest}"}
-                               echo "integration tests:"+ {mavenGoals = "${mvnintTest}"}
-                          }
-                        //"unit tests": { rtMaven.run pom: 'pom.xml', goals: 'test' },
-                        //"integration tests": { rtMaven.run pom: 'pom.xml', goals: 'integration-test' }
+                         "unit tests": { rtMaven.run pom: 'pom.xml', goals: 'test' },
+                        "integration tests": { rtMaven.run pom: 'pom.xml', goals: 'integration-test' }
                     )
                      }
                 }
