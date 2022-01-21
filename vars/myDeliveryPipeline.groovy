8cbac716dcbd9a4c8362ade51ce63f642a97618f
PoojyaShree <87475087+PoojyaShree@users.noreply.github.com>
2022-01-21 11:02:16 +0530

    Update myDeliveryPipeline.groovy

--- vars/myDeliveryPipeline.groovy
+++ vars/myDeliveryPipeline.groovy
@@ -32,7 +32,7 @@ def call(body) {
                                }
                           }
                         }
-                    }
+                    
             stage ('test') {
                  when {
                     anyOf {
