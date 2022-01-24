5f60054426eb22c59394a383ddfcbc80828f94fb
PoojyaShree <87475087+PoojyaShree@users.noreply.github.com>
2022-01-24 19:19:16 +0530

    Update CoolJenkinsfile.groovy

--- vars/CoolJenkinsfile.groovy
+++ vars/CoolJenkinsfile.groovy
@@ -1,8 +1,8 @@
 def call(body) {
 // evaluate the body block, and collect configuration into the object
-def config = [:]
-body.resolveStrategy = Closure.DELEGATE_FIRST
-body.delegate = config
+//def config = [:]
+//body.resolveStrategy = Closure.DELEGATE_FIRST
+//body.delegate = config
 body()
 node( "legacy" ) {
 	echo( "hello world" )
