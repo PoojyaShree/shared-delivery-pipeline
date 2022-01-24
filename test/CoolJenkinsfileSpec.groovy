ae57501f64b8cbced7247daf16de3e0f0f7050fa
PoojyaShree <87475087+PoojyaShree@users.noreply.github.com>
2022-01-24 18:55:30 +0530

    Create CoolJenkinsfileSpec.groovy

---
+++ test/CoolJenkinsfileSpec.groovy
@@ -0,0 +1,9 @@
+def "Jenkinsfile"() {
+	setup:
+		def Jenkinsfile = loadPipelineScriptForTest("com/homeaway/CoolJenkinsfile.groovy")
+	when:
+		Jenkinsfile.run()
+	then:
+		1 * getPipelineMock("node")("legacy", _)
+		1 * getPipelineMock("echo")("hello world")
+}
